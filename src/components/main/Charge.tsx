6330d2e908e043a180772a690cafed911d9edcd8
NPC <1042782229@qq.com>
2023-06-19 23:32:06 +0800

    no message

--- src/components/main/Charge.tsx
+++ src/components/main/Charge.tsx
@@ -136,7 +136,7 @@ export default (props: Props) => {
                 10元<br />12500字(原10500)
               </button>
               <button onClick={() => { getPaycode(20) }} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
-                20元<br />26000字(原22000)
+                20元<br />32000字(原22000)
               </button>
             </div>
           </Show>
