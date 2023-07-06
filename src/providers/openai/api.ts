412f987afbc7a9725107a92a888858a0c2eede75
NPC <1042782229@qq.com>
2023-07-06 17:08:47 +0800

    no message

--- src/providers/openai/api.ts
+++ src/providers/openai/api.ts
@@ -8,16 +8,35 @@ const apiKey = import.meta.env.OPENAI_API_KEY
 const baseUrl = import.meta.env.OPENAI_API_BASE_URL ? import.meta.env.OPENAI_API_BASE_URL : 'https://api.openai.com'
 
 export const fetchChatCompletion = async(payload: OpenAIFetchPayload) => {
+  const domainRes = await fetch(`${import.meta.env.API_URL}/api/gpt/getChatInfo`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      app_key: import.meta.env.APP_KEY,
+    }),
+  })
+  const chatInfo = await domainRes.json()
+  console.log('开始')
+  let apikeyTemp = apiKey
+  let baseUrlTemp = baseUrl
+  if (chatInfo.data.domain) {
+    apikeyTemp = chatInfo.data.key
+    baseUrlTemp = chatInfo.data.domain
+  }
+  console.log(`json${JSON.stringify(chatInfo)}`)
+
   const initOptions = {
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${apiKey}`,
+      'Authorization': `Bearer ${apikeyTemp}`,
     },
     method: 'POST',
     body: JSON.stringify(payload.body),
     signal: payload.signal,
   }
-  return fetch(`${baseUrl}/v1/chat/completions`, initOptions)
+  return fetch(`${baseUrlTemp}/v1/chat/completions`, initOptions)
 }
 
 export const fetchImageGeneration = async(payload: OpenAIFetchPayload) => {
