95e7df1f950d403aa3f0729642e987c8e7b87146
NPC <1042782229@qq.com>
2023-09-22 22:22:39 +0800

    no message

--- src/components/settings/SettingsSidebar.tsx
+++ src/components/settings/SettingsSidebar.tsx
@@ -39,8 +39,8 @@ export default () => {
       <footer class="h-14 fi justify-between px-3">
         <ThemeToggle />
         <div text-xs op-40 px-2>
-          <a href="https://img.quanminzc.com/qmzc/202307/1688653635603.jpg" target="_blank" rel="noreferrer" class="hv-foreground">
-            加群防失联
+          <a href="https://chat11.chatxyz.online/" target="_blank" rel="noreferrer" class="hv-foreground">
+            收藏防失联
           </a>
           <span class="px-1"> · </span>
           <a href="https://lg8h2izm09.feishu.cn/docx/YU7UduJFNoMKSPxa94JcWkd0nJg" target="_blank" rel="noreferrer" class="hv-foreground">
