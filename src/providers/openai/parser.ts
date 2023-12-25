13f834c435b9e6ebfc490d64112640e257d8de1c
1042782229 <1042782229@qq.com>
2023-12-25 15:25:34 +0000

    1

--- src/providers/openai/parser.ts
+++ src/providers/openai/parser.ts
@@ -3,26 +3,27 @@ import type { ParsedEvent, ReconnectInterval } from 'eventsource-parser'
 import type { SettingsPayload } from '@/types/provider'
 
 const consumeWord = async(globalSettings: SettingsPayload, word_num: number, chat_id: string) => {
-  const useRes = await fetch(`${import.meta.env.API_URL}/api/gpt/consumeWord`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Token': globalSettings.authToken as string,
-    },
-    method: 'POST',
-    body: JSON.stringify({
-      model: globalSettings.model as string,
-      type: 'resp',
-      word_num,
-      chat_id,
-      app_key: import.meta.env.APP_KEY,
-    }),
-  }).catch((error) => {
-    console.log(`err${error.message()}`)
-  })
+  try {
+    const useRes = await fetch(`${import.meta.env.API_URL}/api/gpt/consumeWord`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Token': globalSettings.authToken as string,
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        model: globalSettings.model as string,
+        type: 'resp',
+        word_num,
+        chat_id,
+        app_key: import.meta.env.APP_KEY,
+      }),
+    })
+  } catch (err) {
+    console.log(err)
+  }
 
   const res = await useRes.text()
-  console.log(useRes)
-  console.log(res)
+  console.log(`useRes:${useRes}`)
   const resJson = JSON.parse(res)
   console.log(resJson)
   if (resJson.code !== 200)
