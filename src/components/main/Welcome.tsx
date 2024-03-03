2ba813ab2f46145415b8c3f69f35a1c45da0bc49
zlina2931 <142994885+zlina2931@users.noreply.github.com>
2024-03-03 14:17:03 +0000

    1

--- src/components/main/Welcome.tsx
+++ src/components/main/Welcome.tsx
@@ -61,9 +61,9 @@ export default (props: Props) => {
       <div class="flex flex-col w-full max-w-md mx-8 sm:mx-18">
         <Show when={!props.isLogin()}>
           <div class="fi mt-12 ">
-            <span class="text-(2xl transparent) font-extrabold bg-(clip-text gradient-to-r) from-sky-400 to-emerald-600">GPT-4 & CHATXYZ</span>
+            <span class="text-(2xl transparent) font-extrabold bg-(clip-text gradient-to-r) from-sky-400 to-emerald-600">GPT-4 & OHMYAICHAT</span>
           </div>
-          <div mt-1 op-60>欢迎来到CHATXYZ,稳定可靠的AI供应商</div>
+          <div mt-1 op-60>欢迎来到OHMYAICHAT,稳定可靠的AI供应商</div>
           <div op-60>验证邮箱开始使用</div>
           <Login
             setIsLogin={props.setIsLogin}
@@ -80,12 +80,12 @@ export default (props: Props) => {
             <p class="text-lg mb-4">欢迎您成为我们的受邀活动用户！每日签到可免费领取字数,最高单次可领88888字。</p>
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">签到</button>
           </div> */}
-          <a href="https://lg8h2izm09.feishu.cn/docx/YU7UduJFNoMKSPxa94JcWkd0nJg" target="_blank" class="fi gap-2 h-8 text-sm op-60" rel="noreferrer">查看使用说明</a>
-          <a href="https://lg8h2izm09.feishu.cn/docx/FhbmdO3LaoSnadxWWy7cpALSnLe" target="_blank" class="fi gap-2 h-8 text-sm op-60  text-yellow-500" rel="noreferrer">如何区分GPT3.5和GPT4.0</a>
+          <a href="https://pipep2q5ste.feishu.cn/wiki/Ovltwz6oMiOQnxkWYVOcp9ybnyd" target="_blank" class="fi gap-2 h-8 text-sm op-60" rel="noreferrer">查看使用说明</a>
+          <a href="https://pipep2q5ste.feishu.cn/wiki/JBySwujyDintytkdJqNcke2snDh" target="_blank" class="fi gap-2 h-8 text-sm op-60  text-yellow-500" rel="noreferrer">如何区分GPT3.5和GPT4.0</a>
           {/* <a href="https://jiyuimg.wzjo2o.com/vision/202401/1705644527905.jpg" target="_blank" class="fi gap-2 h-8 text-sm op-60  text-yellow-500" rel="noreferrer">加群防失联</a> */}
           {/* <p class="fi gap-2 h-8 text-sm op-60  text-yellow-200" >更新:默认模式更新为单次对话模式,如需连续对话,请点击(「新对话」--「设置」--选择「连续对话」) </p> */}
-          <a href="https://nav.chatxyz.online" target="_blank" class="fi gap-2 h-8 text-sm op-60  text-yellow-500" rel="noreferrer">地址发布页</a>
-          <p class="mt-2 text-xs text-yellow-800">建议收藏永久入口: https://chat11.chatxyz.online 即可获取最新域名！请使用chrome浏览器获得最佳体验效果,其他浏览器可能因不兼容而无法响应</p>
+          {/* <a href="https://nav.chatxyz.online" target="_blank" class="fi gap-2 h-8 text-sm op-60  text-yellow-500" rel="noreferrer">地址发布页</a> */}
+          <p class="mt-2 text-xs text-yellow-800">建议收藏永久入口: https://nav.ohmyaichat.online 即可获取最新域名！请使用chrome浏览器获得最佳体验效果,其他浏览器可能因不兼容而无法响应</p>
           <div class="px-6 py-4 bg-base-100 border border-base rounded-lg">
             <h2 class="text-xs op-30 uppercase my-2">{t('conversations.recent')}</h2>
             <div class="flex flex-col items-start">
